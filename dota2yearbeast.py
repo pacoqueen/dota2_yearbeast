6c70b5f541c0ad4d05535ac60ca9940c5c6cd4aa
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2015-02-25 07:26:05 +0100

    Comments.

--- dota2yearbeast.py
+++ dota2yearbeast.py
@@ -8,6 +8,10 @@ from collections import OrderedDict
 DEBUG = True
 
 def parse_json_data(sdata):
+    """
+    Load data from string (json) and parse dates to local time.
+    Return an ordered dict (reversed in time) with raw data and dates as keys.
+    """
     data = json.loads(sdata)
     # parsing date data
     _data = OrderedDict()
@@ -22,6 +26,9 @@ def parse_json_data(sdata):
     return _data
 
 def get_json_data():
+    """
+    Load pasts events from http://2015.yearbeast.com/ (reddit powah!)
+    """
     c = curl.Curl()
     data = c.get("http://2015.yearbeast.com/history.json")
     data = parse_json_data(data)
@@ -31,11 +38,23 @@ def get_json_data():
     return data
 
 def adivinar(history):
-    last_event = history.keys()[0]
-    delta = last_event - history.keys()[1]
-    return [last_event + delta]
+    """
+    Naive guess of future nexts events based on deltas between past dates and
+    confirmed new one (first one, probably on future).
+    """
+    try:
+        last_event = history.keys()[0]
+        delta = last_event - history.keys()[1]
+    except IndexError:
+        res = ["Not enough data"]
+    else:
+        res = [last_event + delta]
+    return res
 
 def main():
+    """
+    Get json data from past events and try to guess the next one.
+    """
     history = get_json_data()
     next_matches = adivinar(history)
     for nm in next_matches:
